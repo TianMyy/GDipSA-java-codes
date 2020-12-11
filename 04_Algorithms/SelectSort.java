4dccc775ebbc2b622a50ba87764d426c6422ae1c
TianMyy <mingyu_tian@outlook.com>
2020-12-11 16:27:34 +0800

    Add files via upload
    bubble sort and select sort
    bubble sort of String
    binarySearch

--- 04_Algorithms/SelectSort.java
+++ 04_Algorithms/SelectSort.java
@@ -1,12 +1,12 @@
 package day13demo;
 
-public class BubbleSort {
+public class SelectSort {
 	public static void main(String[] args) {
 		int[] arr = {24, 69, 80, 57, 13};
-		System.out.println("Before Bubble Sort:");
+		System.out.println("Before Select Sort:");
 		printArray(arr);
-		System.out.println("After Bubble Sort:");
-		bubbleSort(arr);
+		System.out.println("After Select Sort:");
+		selectSort(arr);
 		printArray(arr);
 	}
 	
@@ -21,13 +21,13 @@ public class BubbleSort {
 		}
 	}
 	
-	public static void bubbleSort(int[] arr) {
+	public static void selectSort(int[] arr) {
 		for(int i = 0; i < arr.length - 1; i++) {
-			for(int j = 0; j < arr.length - 1 - i; j++) {
-				if(arr[j] > arr[j+1]) {
-					int temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
+			for(int j = i+1; j < arr.length; j++) {
+				if(arr[i] > arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
 				}
 			}
 		}
